90e75de6d8ed9832614fb9699c9f49cdf1e6cd29
Julien Ducro <julien.ducro@deskpro.com>
2018-10-01 15:37:36 +0200

    Prevent potential issue with split in formatLabels

--- functions/format_labels.js
+++ functions/format_labels.js
@@ -1,5 +1,7 @@
 const formatLabels = (data) => {
-  if (data.labels) {
+  if (Array.isArray(data.labels)) {
+    data.labels = data.labels.map((item) => item.trim());
+  } else if (data.labels) {
     data.labels = data.labels.split(',').map((item) => item.trim());
   } else {
     data.labels = [];
